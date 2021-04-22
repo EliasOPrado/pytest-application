c8866f25d8f3380b4914a65c6b9d5fee8817b304
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-04-22 16:16:42 -0300

    added test directory

--- api/companies/views.py
+++ api/companies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from .serializers import CompanySerializers
+from .models import Company
+
 
 # Create your views here.
+class CompanyViewSet(ModelViewSet):
+    queryset = Company.objects.all().order_by("-last_update")
+    serializer_class = CompanySerializers
+    pagination_class = PageNumberPagination
--- api/techcompanies/urls.py
+++ api/techcompanies/urls.py
@@ -13,9 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from companies import urls as companies_url
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("", include(companies_url))]
