c78f50d24261d538f0a59352aebfef2b2f49bae0
Oliveira Prado <eliaspradoprofessional@outlook.com>
2021-04-22 21:12:41 -0300

    added test

--- api/companies/tests/test_api.py
+++ api/companies/tests/test_api.py
@@ -1,6 +1,9 @@
 import json
-from unittest import TestCase
 import pytest
+
+from companies.models import Company
+
+from unittest import TestCase
 from django.test import Client
 from django.urls import reverse
 
@@ -11,5 +14,19 @@ class TestGetCompanies(TestCase):
         client = Client()
         companies_url = reverse("companies-list")
         response = client.get(companies_url)
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
+
+    def test_one_company_exists_should_succeed(self) -> None:
+        client = Client()
+        test_company = Company.objects.create(name="Amazon")
+        companies_url = reverse("companies-list")
+        response = client.get(companies_url)
+        response_content = json.loads(response.content)[0]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_content.get("name"), test_company.name)
+        self.assertEqual(response_content.get("status"), "Hiring")
+        self.assertEqual(response_content.get("application_link"), "")
+        self.assertEqual(response_content.get("notes"), "")
+
+        test_company.delete()
